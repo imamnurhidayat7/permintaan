ec0b749733e4cc1d368b20713cf9eebb92a03b24
imam <imamnurhidayat6@gmail.com>
2023-01-27 20:20:10 +0700

    superadmin

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'isSuperAdmin',
         'password',
         'access_token',
         'id_token',
